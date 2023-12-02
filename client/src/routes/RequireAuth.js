d3d5dfc5dc6570d41f7d09d71489ba3622eb80cb
Anirudh Chintha <anirudhchintha95@gmail.com>
2023-12-02 16:51:32 -0500

    restrict auth for art pages

--- client/src/routes/RequireAuth.js
+++ client/src/routes/RequireAuth.js
@@ -1,10 +1,28 @@
+import { Navigate, useLocation } from "react-router-dom";
 import useAuth from "../useAuth";
+import { USER_ROLES } from "../constants";
+
+const isArtSavePage = (pathname) => {
+  const editArtRegex = new RegExp("^/art/[0-9a-fA-F]{24}/edit$", "i");
+  return pathname === "art/create" || editArtRegex.test(pathname);
+};
 
 const RequireAuth = ({ children }) => {
   let auth = useAuth();
 
-  // TODO - If authentication is not successful, redirect users to error page
-  // TODO - use roles to authorize users. For example, only artists can access /art/create
+  let location = useLocation();
+
+  if (!auth?.isLoggedIn) {
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  }
+
+  if (isArtSavePage(location.pathname)) {
+    if (auth?.user?.role === USER_ROLES.ARTIST) {
+      return children;
+    } else {
+      return <Navigate to="/" />;
+    }
+  }
 
   return children;
 };
