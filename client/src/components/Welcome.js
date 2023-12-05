ec6f9c381c65143bf3423d8153c52f8e000b2a74
Anirudh Chintha <anirudhchintha95@gmail.com>
2023-12-05 13:13:11 -0500

    Remove unused code

--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -1,27 +1,8 @@
 import React from "react";
-import { Link } from 'react-router-dom';
-import { Button, Container, Row, Col, Card } from 'react-bootstrap';
-import useAuth from "../useAuth";
+import { Container, Row, Col } from 'react-bootstrap';
 import { WelcomeImage } from "../assets";
-// import backgroundImage from '../path_to_your_image/artistic_background.jpg'; // Update this path to your image file
 
 const Welcome = () => {
-    const auth = useAuth();
-
-    const welcomeCard = (linkTo, buttonText) => (
-        <Card className="text-center" style={{ backgroundColor: 'rgba(255, 255, 255, 0.8)' }}> {/* Semi-transparent background for readability */}
-            <Card.Body>
-                <Card.Title>Welcome Page</Card.Title>
-                <Card.Text>
-                    {auth?.isLoggedIn ? "Go to home if logged in." : "Gives option to login/signup otherwise"}
-                </Card.Text>
-                <Button as={Link} to={linkTo} variant="primary">
-                    {buttonText}
-                </Button>
-            </Card.Body>
-        </Card>
-    );
-
     return (
         <div style={{ 
             backgroundImage: `url(${WelcomeImage})`,
