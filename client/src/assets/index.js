50ab9b8d5e4b8cd0b54a1d1af04c379c55eeb85a
Pooja Mule <poojavm1595@gmail.com>
2023-12-05 00:04:19 -0500

    Redesign welcome page

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -1,3 +1,4 @@
+export { default as WelcomeImage } from "./welcome.png";
 export { default as PlaceholderImage } from "./placeholder-image.jpg";
 export { default as defaultArtistProfile } from "./artist-profile-picture.png";
 export { default as defaultConnoisseurProfile } from "./connoisseur-profile-picture.png";
--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -1,38 +1,47 @@
 import React from "react";
-
 import { Link } from 'react-router-dom';
-import Button from 'react-bootstrap/Button';
+import { Button, Container, Row, Col, Card } from 'react-bootstrap';
 import useAuth from "../useAuth";
-
-
+import { WelcomeImage } from "../assets";
+// import backgroundImage from '../path_to_your_image/artistic_background.jpg'; // Update this path to your image file
 
 const Welcome = () => {
     const auth = useAuth();
-    
-    if (auth?.isLoggedIn) {
-        return (
-            <div>
-                Welcome page - goes to home if logged in. Gives option to login/signup otherwise
-                <div />
-                <Button as={Link} to="/home" variant="primary">
-                    Go to home page
+
+    const welcomeCard = (linkTo, buttonText) => (
+        <Card className="text-center" style={{ backgroundColor: 'rgba(255, 255, 255, 0.8)' }}> {/* Semi-transparent background for readability */}
+            <Card.Body>
+                <Card.Title>Welcome Page</Card.Title>
+                <Card.Text>
+                    {auth?.isLoggedIn ? "Go to home if logged in." : "Gives option to login/signup otherwise"}
+                </Card.Text>
+                <Button as={Link} to={linkTo} variant="primary">
+                    {buttonText}
                 </Button>
-            </div>
-        )
-    }
+            </Card.Body>
+        </Card>
+    );
+
     return (
-        <div>
-            Welcome page - goes to home if logged in. Gives option to login/signup otherwise
-            <div />
-            <Button as={Link} to="/sign-up" variant="primary">
-                Go to sign up page
-            </Button>
-            <div />
-            <Button as={Link} to="/login" variant="primary">
-                Go to login page
-            </Button>
+        <div style={{ 
+            backgroundImage: `url(${WelcomeImage})`,
+            backgroundSize: 'cover',
+            height: '100vh',
+            backgroundPosition: 'center',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center'
+        }}>
+            <Container>
+                <Row className="justify-content-md-center">
+                    <Col md={12} className="text-center">
+                        <h2>Welcome to CanvasCove!</h2>
+                    </Col>
+                </Row>
+            </Container>
         </div>
-    )
+    );
 };
 
 export default Welcome;
